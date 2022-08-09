0af01df9d1db1c20f527fa5cb707bed346983483
Carrie Platten <carriep@Carries-MacBook-Pro.local>
2022-08-09 22:49:04 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import flag from "./images/flag.jpeg";
+import "./App.css";
+import party from "party-js";
 
 function App() {
+
+  // const 
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>🏳️‍🌈</h1>
       </header>
+      <main>
+        <button 
+         onClick={(e) => party.confetti(e.target)}
+         className="flag-button"
+        >
+          Click me!
+        </button>
+        <br></br>
+        <br></br>
+        <button 
+         onClick={(e) => party.sparkles(e.target)}
+         className="bit-gay-button"
+        >
+          bit gay!
+        </button>
+        {/* <img src={flag} 
+        // className="App-logo" 
+        alt="logo" /> */}
+      </main>
     </div>
   );
 }
